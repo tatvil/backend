1173c901e4180dedc105ab00379878fec08cd7bb
Tatiana Villa Ema <tatiana@tecnologia-facil.es>
2026-02-22 00:32:53 +0100

    mapeando tablas

--- src/main/java/es/tatvil/backend/controllers/WeatherController.java
+++ src/main/java/es/tatvil/backend/controllers/WeatherController.java
@@ -5,6 +5,7 @@ import es.tatvil.backend.repositories.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -23,8 +24,8 @@ public class WeatherController {
     @GetMapping("/filter")
     public List<Weather> getWeatherFiltered(
         @RequestParam String ciudad,
-        @RequestParam String desde,
-        @RequestParam String hasta
+        @RequestParam LocalDate desde,
+        @RequestParam LocalDate hasta
     ) {
         return weatherRepository.findByCiudadAndFechaBetween(ciudad, desde, hasta);
     }
--- src/main/java/es/tatvil/backend/repositories/WeatherRepository.java
+++ src/main/java/es/tatvil/backend/repositories/WeatherRepository.java
@@ -2,12 +2,14 @@ package es.tatvil.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 
 import es.tatvil.backend.entities.Weather;
 
 @Repository
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
-    // Esto genera automáticamente el SQL: SELECT * FROM weather WHERE ciudad = ? AND fecha BETWEEN ? AND ?
-    List<Weather> findByCiudadAndFechaBetween(String ciudad, String desde, String hasta);
+public interface WeatherRepository extends JpaRepository<Weather, Integer> {
+    // Spring entenderá que debe filtrar por 'ciudad' y por el campo 'fecha'
+    List<Weather> findByCiudadAndFechaBetween(String ciudad, LocalDate desde, LocalDate hasta);
 }
